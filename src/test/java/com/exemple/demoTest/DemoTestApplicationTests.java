6fcd5f1748653ddd5f141a67d3bfe9c58a8643f1
ZIED DAMMAK <zied.dammak@enis.tn>
2023-12-04 14:26:04 +0100

    second commit

--- src/test/java/com/exemple/demoTest/DemoTestApplicationTests.java
+++ src/test/java/com/exemple/demoTest/DemoTestApplicationTests.java
@@ -1,13 +1,21 @@
 package com.exemple.demoTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 @SpringBootTest
 class DemoTestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
+
 
 }
